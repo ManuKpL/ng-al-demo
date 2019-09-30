dd2be913e1a667f6bd9d4769e7b42095329e52eb
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-10-01 00:51:37 +0200

    :books: comments for main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import assets
 import './favicon.ico';
 import './styles.scss';
 
+// import angular module to be used as application
+import { AppModule } from './app/app.module';
+
+// declare the global variable provided by webpack define plugin
+// to allow usage in TS file without compiler error
 declare const PRODUCTION: boolean;
 if (PRODUCTION) {
-  enableProdMode();
+  enableProdMode(); // disable angular development mode
 }
 
 // start an angular module as an application for the browser
