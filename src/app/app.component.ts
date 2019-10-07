8e0f7ba48d46ea9094ff6656e4e814d490127b9b
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-10-07 14:47:36 +0200

    :art: changed app name in app component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sw-root',
   template: '<p>Hello from Angular</p>',
 })
 export class AppComponent {}
