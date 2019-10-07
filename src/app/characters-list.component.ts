5bc98afeb6092141fb7600f0208df2100b74da69
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-10-07 17:28:47 +0200

    :sparkles: added style to list component

--- src/app/characters-list.component.ts
+++ src/app/characters-list.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sw-characters-list',
   templateUrl: './characters-list.component.html',
+  styleUrls: ['./characters-list.component.scss'],
 })
 export class CharactersListComponent {
+  public target = '_blank';
+
   public character = {
     id: 1,
     name: 'Luke Skywalker',
     gender: 'male',
     url: 'https://swapi.co/api/people/1/',
+    image: 'https://vignette.wikia.nocookie.net/fr.starwars/images/f/fa/Luke_TLJ.jpg/revision/latest?cb=20180110093251',
   };
 }
