962ed635c153befc5f4af735497e693e6aace035
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-10-07 16:16:36 +0200

    :art: added character data to template

--- src/app/characters-list.component.ts
+++ src/app/characters-list.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'sw-characters-list',
   templateUrl: './characters-list.component.html',
 })
-export class CharactersListComponent {}
+export class CharactersListComponent {
+  public character = {
+    id: 1,
+    name: 'Luke Skywalker',
+    gender: 'male',
+    url: 'https://swapi.co/api/people/1/',
+  };
+}
